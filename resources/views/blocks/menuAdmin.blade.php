3d812ce070a62626a87a840165f6024a67b165b0
johann creneguy <johann.creneguy@gmail.com>
2017-01-25 21:19:23 +0000

    [update] design welcome page + phonebook page

--- resources/views/blocks/menuAdmin.blade.php
+++ resources/views/blocks/menuAdmin.blade.php
@@ -4,4 +4,4 @@
 <li><a href="/group/">Groupes</a></li>
 <li><a href="/module/">Cours</a></li>
 <li><a href="/quizz/">Quizz</a></li>
-<li><a href="/register">Créer compte</a></li>
+<li><a href="/register/">Créer compte</a></li>
