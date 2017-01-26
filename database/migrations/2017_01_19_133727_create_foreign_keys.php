fd37bf65966fbb9d2f1fb955de60c2aebc7c8d12
Guillaume <undogit@gmail.com>
2017-01-26 12:24:24 +0000

    [Add] Dynamic enterclassroom view

--- database/migrations/2017_01_19_133727_create_foreign_keys.php
+++ database/migrations/2017_01_19_133727_create_foreign_keys.php
@@ -115,6 +115,6 @@ class CreateForeignKeys extends Migration {
         Schema::table('sessions', function(Blueprint $table) {
             $table->dropForeign('sessions_answer_id_foreign');
         });
-    
+
 	}
 }
