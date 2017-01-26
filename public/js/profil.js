0b3ade934f6877d633ca3ba57cbd0a995a4a67f8
johann creneguy <johann.creneguy@gmail.com>
2017-01-26 11:08:26 +0000

    [add] design for user list for admin + generik picture for profil

---
+++ public/js/profil.js
@@ -0,0 +1,7 @@
+/**
+ * Created by apprenant on 26/01/17.
+ */
+console.log($('.avatar img').attr('src'));
+if($('.avatar img').attr('src') == '/uploads/images/users/'){
+    $('.avatar img').attr('src','/img/avatar-generik.png');
+ }
